f67e31dcb27f87ebbb3d73b6ee66b86487cf09f9
Bruno Santos <bmrosantos@gmail.com>
2015-06-24 23:38:27 -0400

    Introduced parametarized test
    Added example that exercises the special case of a JUnit pojo that has a specialized constructor.
    
    cola-tests issue #21

---
+++ cola-maven-plugin-test/src/test/java/com/github/bmsantos/maven/cola/ParameterizedColaTest.java
@@ -0,0 +1,78 @@
+package com.github.bmsantos.maven.cola;
+
+import static java.util.Arrays.asList;
+import static java.util.Objects.requireNonNull;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import com.github.bmsantos.core.cola.story.annotations.Feature;
+import com.github.bmsantos.core.cola.story.annotations.Given;
+import com.github.bmsantos.core.cola.story.annotations.Then;
+import com.github.bmsantos.core.cola.story.annotations.When;
+
+@RunWith(Parameterized.class)
+public class ParameterizedColaTest extends BaseColaTest {
+
+    private final Integer inputNumber;
+    private final Boolean expectedResult;
+    private boolean actual;
+
+    public ParameterizedColaTest(final Integer inputNumber, final Boolean expectedResult) {
+        this.inputNumber = inputNumber;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<?> primeNumbers() {
+        return asList(new Object[][] {
+            { 2, true },
+            { 6, false },
+            { 19, true },
+            { 22, false },
+            { 23, true }
+        });
+    }
+
+    @Feature
+    private final String feature =
+    "Feature: Detect prime numbers\n"
+        + "Scenario: Should detect prime numbers\n"
+        + "Given a number\n"
+        + "And a prime number guess\n"
+        + "When validated for prime\n"
+        + "Then guess should match validation result";
+
+    @Given("a number")
+    public void givenANumber() {
+        requireNonNull(inputNumber);
+    }
+
+    @Given("a prime number guess")
+    public void givenAGuess() {
+        requireNonNull(expectedResult);
+    }
+
+    @When("validated for prime")
+    public void whenValidated() {
+        actual = validate(inputNumber);
+    }
+
+    @Then("guess should match validation result")
+    public void verifyGuess() {
+        assertThat(actual, is(expectedResult));
+    }
+
+    public Boolean validate(final Integer primeNumber) {
+        for (int i = 2; i < primeNumber / 2; i++) {
+            if (primeNumber % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
