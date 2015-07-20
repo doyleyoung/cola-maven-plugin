9e44a4ccfc3e3a1f828138000ac67e1adf8c137c
Bruno Santos <bmrosantos@gmail.com>
2015-07-20 18:52:46 -0400

    Error method injection example
    Add test that can cause an injection of cola-tests error reporting method.

---
+++ cola-maven-plugin-test/src/test/java/com/github/bmsantos/maven/cola/CompileErrorNotificationColaTest.java
@@ -0,0 +1,9 @@
+package com.github.bmsantos.maven.cola;
+
+
+public class CompileErrorNotificationColaTest extends BaseColaTest {
+
+    // To test this class remove comment
+    // @Feature
+    private final String feature = "No Feature Here!";
+}
